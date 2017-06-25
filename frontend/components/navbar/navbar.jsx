38aa6e5cf7a2889186cd2160fb366e140e6fd96d
Maggie Liu <mliu0805@gmail.com>
2017-06-25 00:53:03 -0400

    Add box shadow to hover

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,16 +4,20 @@ class NavBar extends React.Component {
   render() {
     return(
       <header className='nav-bar'>
-        <h1>JO</h1>
-        <ul className='links'>
-          <li className='journal'></li>
-          <li className='neighborhood'></li>
-          <li className='amenities'></li>
-          <li className='residences'></li>
-          <li className='availabilities'></li>
-          <li className='team'></li>
-          <li className='contact'></li>
-        </ul>
+        <div className='full-nav'>
+          <h1>JO</h1>
+          <ul className='links'>
+            <li className='journal'></li>
+            <li className='neighborhood'></li>
+            <li className='amenities'></li>
+            <li className='residences'></li>
+            <li className='availabilities'></li>
+            <li className='team'></li>
+            <li className='contact'></li>
+          </ul>
+        </div>
+
+        <div className='mobile-nav'></div>
       </header>
     );
   }
