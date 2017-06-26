630aa9baa494d1ad427bbddc5e03833258298c8f
Maggie Liu <mliu0805@gmail.com>
2017-06-26 18:04:34 -0400

    Fix icons on post

--- frontend/util/api_util.js
+++ frontend/util/api_util.js
@@ -1,6 +1,6 @@
 const instagramData = require('../../app/assets/javascripts/instagram.json')
 
-const authToken = 'EAACEdEose0cBACm0Inyujnk85cOdcyNQ8CAgei55U40F7g0YcWo8sT045zaZBTidXcgZAxb2Uzs3VFZCgcnuJSSN23ct17m8ZC369sBJYQfjeuDUZBf6NqZC9pLhfUcDZBvj5cEG6LseKqQCnQcL2J4VkfsLhb7SQdkOF2f1PBGZCe8iVBRzY1bJfqbABVZCEoWgZD';
+const authToken = 'EAACEdEose0cBALd91f86b3q5tLoP23C6dhmhGK1rbMkkS6ZCZBBzlZCZAaauxGw663rxP7LjVNKgaZAp1tkUlSV2kEZCLsL0j86JiZAi4RoRYwLek5eawC9cezZC225DVcuet8ERmUE1NrgxYQLUAPaqxqZBiitCLfddkZCOi3QKVpfd9ijPmCUzRjZCy8Gk7vXF4wZD';
 
 export const fetchAllFacebookPosts = () => {
   return(
