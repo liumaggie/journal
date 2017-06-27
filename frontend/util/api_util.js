96c0d78e3c4b7d84287c21578d928bad9b9d8945
Maggie Liu <mliu0805@gmail.com>
2017-06-27 00:09:32 -0400

    Update fb token

--- frontend/util/api_util.js
+++ frontend/util/api_util.js
@@ -1,6 +1,6 @@
 const instagramData = require('../../app/assets/javascripts/instagram.json')
 
-const authToken = 'EAACEdEose0cBAAOZA0Uqxnjy0Jl7F7w6sKYXVZC934GY9TSTM6S1WrZCy4fcwkFgJswBIyqpmjhbhcfAORlSB9B85qOgvt9pHMyYrx3Pfk7QPNu6zEVZCn6qOjNegzxTc6JWAkFZAL4FPNb5AZAZB8ONXXgtCPc46kokvTVJgydhynhZAeR3HeZCILlJ5541YhoEZD';
+const authToken = 'EAABrLZCqb5ocBAMPjP0ZC6CTkTiJCkjCjULY0ORJfbgJSmnfxj8bNO8ZA17N3Lhv5DolTsLl1ZAGK7l93kROmbcQZCX8GXTvvHNa5thcqYJHtlUZB2ZAsPpCYRIWOTg8O2lz5nLQpQ8QoZAlmaUocmZA3O3ZANU6eW0RQZD';
 
 export const fetchAllFacebookPosts = () => {
   return(
@@ -15,27 +15,7 @@ export const fetchAllInstagramPosts = () => {
   return instagramData.items;
 };
 
-//
-// export const fetchSingleFacebookPost = (id) => {
-//   return(
-//     $.ajax({
-//       method: 'GET',
-//       url: `https://graph.facebook.com/v2.9/${id}?oauth_token=${authToken}`
-//     })
-//   );
-// };
-//
-// $.ajax({
-//   method: 'GET',
-//   url: `https://www.instagram.com/journalsquared/media&callback=?`,
-//   dataType: "jsonp",
-//   contentType: "application/json; charset=utf-8",
-//   jsonpCallback: "callback",
-//   success: function (data) {
-//             $('div').html(JSON.stringify(data));
-//         }
-// });
-//
+
 // $.ajax({
 //     url: "https://api.instagram.com/v1/users/2318793052/?access_token=30117210.2ccad70.01e429ee53e94c68a4ddba175d099179&callback=?",
 //     dataType: "jsonp",
