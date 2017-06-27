43cb1b75d394446af201c390855ec8a5c96489c4
Maggie Liu <mliu0805@gmail.com>
2017-06-26 23:05:21 -0400

    Make posts responsive

--- frontend/util/api_util.js
+++ frontend/util/api_util.js
@@ -1,6 +1,6 @@
 const instagramData = require('../../app/assets/javascripts/instagram.json')
 
-const authToken = 'EAACEdEose0cBALd91f86b3q5tLoP23C6dhmhGK1rbMkkS6ZCZBBzlZCZAaauxGw663rxP7LjVNKgaZAp1tkUlSV2kEZCLsL0j86JiZAi4RoRYwLek5eawC9cezZC225DVcuet8ERmUE1NrgxYQLUAPaqxqZBiitCLfddkZCOi3QKVpfd9ijPmCUzRjZCy8Gk7vXF4wZD';
+const authToken = 'EAACEdEose0cBAAOZA0Uqxnjy0Jl7F7w6sKYXVZC934GY9TSTM6S1WrZCy4fcwkFgJswBIyqpmjhbhcfAORlSB9B85qOgvt9pHMyYrx3Pfk7QPNu6zEVZCn6qOjNegzxTc6JWAkFZAL4FPNb5AZAZB8ONXXgtCPc46kokvTVJgydhynhZAeR3HeZCILlJ5541YhoEZD';
 
 export const fetchAllFacebookPosts = () => {
   return(
